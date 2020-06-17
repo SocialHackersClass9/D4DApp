89fae5aca8d01284a9aed72d8854f00b8e73309f
Evina Mouselimi <evinamous@gmail.com>
2020-06-17 21:43:44 +0300

    Fixed AccessibleRegistration (Form1). Sports list missing.

---
+++ frontend/src/views/AccesibleRegistration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GeneralInfoForm from "../components/GeneralInfoForm";
+
+export default function AccessibleRegistration() {
+    return (
+        <div>
+            <GeneralInfoForm />
+        </div>
+    )
+};
+
+

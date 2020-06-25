da4b2c1aa5e52647c43b358bb7e1042005bffb04
sobhanessi <sobhanessi@gmail.com>
2020-06-25 12:49:41 +0300

    working on frontend

--- frontend/src/views/Search.js
+++ frontend/src/views/Search.js
@@ -7,9 +7,16 @@ class Search extends React.Component {
             locations: [],
             sports: [],
             loadingLocations: true,
+            loadingSports: true,
+            favSport: "*",
+            favLocation: "*",
+            matches: [],
+            loadinLocationMatches: true,
+            loadingSportMatches : true
             
         }
         this.componentDidMount = this.componentDidMount.bind(this)
+        this.handleChange = this.handleChange.bind(this);
     }
     // change() {
 
@@ -37,26 +44,42 @@ class Search extends React.Component {
         
     }
 
+    handleChange(e){
+        const {name,value} = e.target
+        this.setState({[name]:value})
+
+        const response = fetch(`http://localhost:3001/location=${this.state.favLocation}-sport=${this.state.favSport}`)
+        const json = response.json()
+        const data = this.setState({user: data,loadinMatches : false})
+
+
+    }
+
 
 
     render() {
 
-        const locations = this.state.loadingLocations ? "loading" : this.state.locations.map(city => <option>{city.city}</option>)
+        const locations = this.state.loadingLocations ? "loading" : this.state.locations.map(city => <option value={city.city}>{city.city}</option>)
     
         const sports = this.state.loadingSports ? "loading" : this.state.sports.map(sport => <option value={sport.sport}>{sport.sport}</option>)
 
+    const matches = this.state.loadingMatches ? "select something" : this.state.matches.map(match => <div><p>{match.name}</p><p>{match.family}</p></div>)
+
+        console.log("state : ",this.state)
         return (
             <div>   
                 <p>
-                    <select>
+                    <select name="favLocation" onChange={this.handleChange} value={this.state.favLocation}>
                         {locations}
                     </select>
 
-                    <select>
+                    <select name="favSport" onChange={this.handleChange} value={this.state.favSport}>
                         {sports}
                     </select>
                     
-                    
+                    <div>
+                        {matches}
+                    </div>
                     
                 </p>
                 

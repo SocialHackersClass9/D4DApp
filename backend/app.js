5a9bd73194554b7b051ce32b7c27b334be13cdd3
sobhanessi <sobhanessi@gmail.com>
2020-07-05 11:29:02 +0300

    changes in query for search page

--- backend/app.js
+++ backend/app.js
@@ -56,24 +56,94 @@ app.get('/search/regions', (req, res) => {
 
 app.get('/search/region=:region_id-sport=:sport', (req, res) => {
     let sql;
-    if (req.params.region_id === "*" && req.params.sport === "*") {
-        sql = "SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id"
+    // if (req.params.region_id === "*" && req.params.sport === "*") {
+    //     sql = "SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id"
+    // }
+    // else if (req.params.region_id === "*") {
+    //     sql = `SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id WHERE sports.id=${req.params.sport}`
+    // }
+    // else if (req.params.sport === "*") {
+    //     sql = `SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id WHERE instructors.region_id=${req.params.region_id}`
+    // }
+    // else {
+    //     sql = `SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id WHERE instructors.region_id=${req.params.region_id} AND sports.id=${req.params.sport}`
+    // }
+    let instructors, sports, locations, inst_locts;
+    if (req.params.region_id === "all" && req.params.sport === "all") {
+        sql = `select id ,first_name , last_name , region_id from instructors`
+        con.query(sql, (err, ins) => {
+            if (err) console.log(err);
+            // console.log("instructors : ", instructors)
+            instructors = ins;
+            sql2 = `select instructor_id ,sport_id , name from instructors_sports ins inner join sports s on ins.sport_id=s.id`
+            con.query(sql2, (err, ins_sport) => {
+                if (err) console.log(err);
+                // console.log("sports : ", sports)
+                sports = ins_sport;
+                sql3 = `select instructor_id , location_id , name from instructors_locations il inner join locations l on il.location_id=l.id`
+                con.query(sql3, (err, il) => {
+                    if (err) console.log(err);
+                    // console.log("il : ", il)  
+                    locations = il;
+                    data = instructors.forEach(instructor => {
+                        let inst_locs = locations.filter(loc => loc.instructor_id == instructor.id);
+                        // res.json(locs)
+                        instructor.locations = inst_locs;
+                        let inst_sports = sports.filter(sport => sport.instructor_id == instructor.id);
+                        instructor.sports = inst_sports;
+                        console.log(instructor)
+                    });
+                    res.json(data)
+                })
+            })
+        })
     }
-    else if (req.params.region_id === "*") {
-        sql = `SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id WHERE sports.id=${req.params.sport}`
+    else if (req.params.sport === "all") {
+        sql = `select id , first_name , last_name from instructors where region_id=${req.params.region_id}`
+        con.query(sql, (err, inst) => {
+            if (err) console.log(err);
+            instructors = inst;
+            sql2 = `select id , name from locations where region_id=${req.params.region_id}`
+            con.query(sql2, (err, locts) => {
+                if (err) console.log(err);
+                locations = locts;
+                sql3 = `select instructor_id , location_id from instructors_locations`
+                con.query(sql3, (err, inst_locs) => {
+                    if (err) console.log(err);
+                    inst_locts = inst_locs;
+                    sql4 = `select instructor_id , sport_id, name from instructors_sports ins inner join sports on ins.sport_id=sports.id`
+                    con.query(sql4, (err, sport) => {
+                        if (err) console.log(err);
+                        sports = sport;
+
+                        // console.log("instructors", instructors)
+                        // console.log("locations", locations)
+                        // console.log("inst_locations", inst_locts)
+                        // console.log("sports", sports)
+
+                        instructors.forEach(instructor => {
+                            let instructor_locts = inst_locts.filter(il => il.instructor_id === instructor.id);
+                            let s = sports.filter(sport => sport.instructor_id === instructor.id)
+                            instructor.sports = s;
+                            let locs = instructor_locts.forEach(ins_loc => {
+                                let locs = locations.filter(loc => loc.id === ins_loc.location_id)
+                                instructor.locations = locs;
+                                console.log(instructor)
+                            })
+
+
+
+                        })
+                    })
+                })
+            })
+        })
     }
-    else if (req.params.sport === "*") {
-        sql = `SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id WHERE instructors.region_id=${req.params.region_id}`
-    }
-    else {
-        sql = `SELECT instructors.first_name , instructors.last_name , sports.name , sports.name_gr FROM instructors,sports INNER JOIN instructors_sports ON instructors_sports.id=sports.id WHERE instructors.region_id=${req.params.region_id} AND sports.id=${req.params.sport}`
-    }
-    con.query(sql, (err, result) => {
-        if (err) console.log(err);
-        res.json(result)
-    })
+
 
 })
+
+
 
 app.post('/login', (req, res) => {
 
@@ -86,6 +156,11 @@ app.post('/login', (req, res) => {
         res.json(result)
     })
 })
+
+// app.post('/register/instructor', (req, res) => {
+//     let sql = `INSERT INTO instructors (first_name , last_name , year_of_birth , street_number , street , )`
+// })
+
 //for stefanos
 app.post('/contact', (req, res) => {
 

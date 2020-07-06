ef2d1d681a652d81a6c68970f1ca37e3a9be2484
Evina Mouselimi <evinamous@gmail.com>
2020-07-06 21:30:18 +0300

    front-end for search page(work-in-progress)

--- frontend/src/views/InstructorProfileContact.js
+++ frontend/src/views/InstructorProfileContact.js
@@ -6,7 +6,6 @@ export default function InstructorProfileContact() {
     const url = process.env.REACT_APP_API_URL + '/instructor';
 
     useEffect(() => {
-
         fetch(url)
             .then(res => res.json())
             .then(data => {
@@ -22,7 +21,7 @@ export default function InstructorProfileContact() {
                 <div className="image">
                     <div className="circle-1" />
                     <div className="circle-2" />
-                    <img src={"https://100dayscss.com/codepen/jessica-potter.jpg"} width={300} height={300} alt="Instructor's photo" />
+                    <img src={"https://100dayscss.com/codepen/jessica-potter.jpg"} width={300} height={300} alt="Instructor" />
                 </div>
                 <form className="contact1-form validate-form">
                     <span className="contact1-form-title">
@@ -30,7 +29,8 @@ export default function InstructorProfileContact() {
             </span>
                     <table className="table table-borderless d-flex justify-content-center">
                         <tbody >
-                            <tr scope="row ">{instructor.first_name} {instructor.last_name}
+                            <th scope="row "></th>
+                            <tr>{instructor.first_name} {instructor.last_name}
 
                             </tr>
                             <tr>
--- frontend/src/views/Search.js
+++ frontend/src/views/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LocationsFilter from '../Components/LocationsFilter'
+//import LocationsFilter from '../Components/LocationsFilter'
 class Search extends React.Component {
     constructor() {
         super();
@@ -22,7 +22,7 @@ class Search extends React.Component {
 
     // }
 
-    componentDidMount(){
+    componentDidMount() {
         fetch("http://localhost:3001/search/locations")
             .then(response => response.json())
             .then(data => {
@@ -31,8 +31,8 @@ class Search extends React.Component {
                     loadingLocations: false
                 })
             });
-        
-        
+
+
         fetch("http://localhost:3001/search/sports")
             .then(response => response.json())
             .then(data => {
@@ -41,27 +41,27 @@ class Search extends React.Component {
                     loadingSports: false
                 })
             });
-        
+
     }
 
-    handleChange(e){
-        const {name,value} = e.target
-        this.setState({[name]:value})
+    handleChange(e) {
+        const { name, value } = e.target
+        this.setState({ [name]: value })
 
-        let l = "",s = "",url = "";
+        let l = "", s = "", url = "";
         let matches = []
-        if(e.target.name === "favLocation"){
+        if (e.target.name === "favLocation") {
             l = e.target.value;
         }
-        if(e.target.name === "favSport") {
+        if (e.target.name === "favSport") {
             s = e.target.value;
         }
-        
+
         // if(l.length & s.length > 0){
-            url = `http://localhost:3001/location=${l}-sport=${s}`
-            fetch(url)
-                .then(res => res.json())
-                .then(data => matches = data)
+        url = `http://localhost:3001/location=${l}-sport=${s}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => matches = data)
         //}
         console.log(url)
     }
@@ -71,37 +71,48 @@ class Search extends React.Component {
     render() {
 
         const locations = this.state.loadingLocations ? "loading" : this.state.locations.map(city => <option value={city.city}>{city.city}</option>)
-    
+
         const sports = this.state.loadingSports ? "loading" : this.state.sports.map(sport => <option value={sport.sport}>{sport.sport}</option>)
 
         //const matches = this.state.loadingMatches ? "select something" : this.state.matches.map(match => <div><p>{match.name}</p><p>{match.family}</p></div>)
-        
-        
-        return (
-            <div>   
-                <p>
-                    <select name="favLocation" onChange={this.handleChange} value={this.state.favLocation}>
-                        <option value="None">None</option>
-                        {locations}
-                    </select>
 
-                    <select name="favSport" onChange={this.handleChange} value={this.state.favSport}>
-                    <option value="None">None</option>
-                        {sports}
-                    </select>
-                    
-                    {/* <div>
-                        {matches}
-                    </div> */}
-                    
-                </p>
-                
+        return (
+            <div className="container">
+                <div className="row row-header align-self-center">
+                    <div className="col-12 col-sm-12 col-xs-12 text-center">
+                        <h1 className="text-center"> Instructor Search</h1>
+                    </div>
+                </div>
+                <div className="searchFeat">
+                    <div className="row align-self-center">
+                        <div className="col-md-6 text-center">
+                            <h2 className="text-center">Search via Location</h2>
+                        </div>
+                        <div className="col-md-6 text-center align-self-center">
+                            <select name="favLocation" onChange={this.handleChange} value={this.state.favLocation}>
+                                <option value="None">None</option>
+                                {locations}
+                            </select>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 text-center align-self-center">
+                            <h2 className="text-center">Search via Sports</h2>
+                        </div>
+                        <div className="col-md-6 text-center align-self-center">
+                            <select name="favSport" onChange={this.handleChange} value={this.state.favSport}>
+                                <option value="None">None</option>
+                                {sports}
+                            </select>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-12 col-xs-12 text-center align-self-center'>
+                        <button type="button" className="btn btn-primary btn-lg"> CLICK TO SEARCH</button>
+                    </div>
+                </div>
             </div>
-               
         )
     }
 }
-
-
 
 export default Search;

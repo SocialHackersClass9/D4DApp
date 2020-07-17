6dfb27c39725e2953a0e84c8010a588a4b926657
amirhossein <mosaviamirhoseyn9@gmail.com>
2020-07-17 13:47:32 +0300

    merged by dev branch

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const env = require('./env')
 // const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const mysql = require('mysql');
-
-
+const fileUpload = require('express-fileupload');
+const { get } = require("./env");
 env.get();
+
 const port = process.env.PORT
 
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json());
+
 //for stefanos
 app.use(express.urlencoded({ extended: true }))
 const mailtransport = nodemailer.createTransport({
@@ -27,7 +30,8 @@ let con = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'd4d'
+    // database: 'd4d'
+    database: 'D4DApp'
 })
 
 
@@ -38,6 +42,7 @@ app.get('/', (req, res) => {
 })
 
 ////////////////////////
+
 app.get('/sports', (req, res) => {
     let sql = `SELECT id, name FROM sports`
     con.query(sql, (err, result) => {
@@ -135,6 +140,109 @@ app.post('/contact', (req, res) => {
     }
     send(msg);
 })
+
+
+
+
+app.post('/instructors/upload/data', function (req, resp, next) {
+
+    const { email, user_name, password, first_name, last_name, year_of_birth, region_id ,phone , education, gender, street, street_number ,locations ,sports , zip, occupation, details, photo} = req.body;
+   
+        function insert (input_name_val , insert_name) {
+            let instructor_id = null ; 
+                con.query(insert_name, [input_name_val], function (err, result) {
+                    if  (err) throw err;    
+                    var sql = 'SELECT id FROM instructors WHERE email = ? AND user_name = ?';
+                    con.query(sql, [email , user_name], function (err, result) {
+                        if (err) throw err;
+                        instructor_id = parseInt(result[0].id);    
+                        var sql1 = "INSERT INTO instructors_locations (instructor_id, location_id) VALUES ?";
+                        locations.map(item => {
+                        var locations = [[instructor_id,item]]
+                        con.query(sql1, [locations], function (err, result) {
+                            if (err) throw err;
+                          });
+                        })
+                
+                        var sql2 = "INSERT INTO instructors_sports (instructor_id, sport_id) VALUES ?";
+                        sports.map(item => {
+                        var sports = [[instructor_id,item]]
+                        con.query(sql2, [sports], function (err, result) {
+                            if (err) throw err;
+                          });
+                        })
+                
+                    });
+            });
+            console.log('database uploaded');
+            resp.json({ msg: 'Registration have been complited' })
+        }
+        
+            var input_name_val1 ;
+            var insert_name1 ;
+
+            if(region_id.length != 0 ){
+                var input_name_val1 = [[email, user_name, password, first_name, last_name, year_of_birth, region_id ,phone , education, gender, street, street_number, zip, occupation, details, photo]];
+                var insert_name1 = "INSERT INTO instructors(email, user_name, password, first_name, last_name, year_of_birth, region_id ,phone , education, gender, street, street_number, zip, occupation, details, photo) VALUES ?";
+                insert(input_name_val1, insert_name1);
+            }else{
+                var input_name_val2 = [[email, user_name, password, first_name, last_name, year_of_birth ,phone , education, gender, street, street_number, zip, occupation, details, photo]];
+                var insert_name2 = "INSERT INTO instructors(email, user_name, password, first_name, last_name, year_of_birth ,phone , education, gender, street, street_number, zip, occupation, details, photo) VALUES ?";
+                insert(input_name_val2, insert_name2)
+            }
+    });
+    
+
+    
+    app.post('/instructors/upload/img', fileUpload(), (req, res) => {
+
+            if (req.files === null) {
+                return res.status(400).json({ msg: 'No file uploaded' });
+            }
+    
+            const file = req.files.file;
+    
+            file.mv(`${__dirname}/Instructors_profile_images/${file.name}`, err => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send(err);
+                }
+                res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });            
+                console.log('image uploaded');
+
+            });
+    });
+    
+    
+    app.get('/instructors/used', function (req, resp, next) {
+    
+            con.query("SELECT email,user_name FROM instructors", function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+                const emailuser = result;
+                resp.json({ emailuser: emailuser })
+                next();
+            })
+    })
+   
+    app.get('/locations', function (req, resp, next) {
+    
+            con.query("SELECT * FROM locations", function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+                const locations = result;
+                resp.json(locations)
+                next();
+            })
+    })
+    
+////////////////////////
+
+
+app.get('/', (req, res) => {
+    res.json({ "greeting": "Hello World!" });
+})
+
 
 ////////////////////////
 

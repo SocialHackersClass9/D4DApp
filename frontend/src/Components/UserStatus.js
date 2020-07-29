e8906e18d017bcfb01ccfc8c45aad3bd59de9dc7
Evina Mouselimi <evinamous@gmail.com>
2020-07-29 13:38:27 +0300

    logo2

--- frontend/src/Components/UserStatus.js
+++ frontend/src/Components/UserStatus.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState, useContext} from "react";
+import { useState, useContext } from "react";
 import apis from '../apis.js';
 import AppContext from '../context.js';
 import { Nav } from 'react-bootstrap';
@@ -50,40 +50,41 @@ const Register = () => {
 
 const Login = () => {
     const [show, setShow] = useState(false);
-    const [values, setValues] = useState({email: "george@test.com", password: "12345"});
+    const [values, setValues] = useState({ email: "george@test.com", password: "12345" });
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const handleChangeEmail = function (e) {
-        setValues({...values, email: e.target.value});
+        setValues({ ...values, email: e.target.value });
     }
     const handleChangePassword = function (e) {
-        setValues({...values, password: e.target.value});
+        setValues({ ...values, password: e.target.value });
     }
     const context = useContext(AppContext);
     const handleSubmit = function (e) {
         e.preventDefault();
         console.log(values);
         apis.post('/login', values)
-        .then(data => {
-            if (data.is_authenticated) {
-                console.log(data);
-                context.setContext({user: data.user});
-                setShow(false);
-            } else {
-                alert("User name or password is wrong. Please try again.");
-            }
-        });
+            .then(data => {
+                if (data.is_authenticated) {
+                    console.log(data);
+                    context.setContext({ user: data.user });
+                    setShow(false);
+                } else {
+                    alert("User name or password is wrong. Please try again.");
+                }
+            });
     }
 
     return (
         <Styles>
             {(context.user == null) &&
-            <Button variant="primary" onClick={handleShow}>Login</Button>
+                <Button variant="primary" onClick={handleShow}>Login</Button>
             }
             {(context.user != null) &&
-                <Nav.Item><Nav.Link href="/">{ context.user.user_name }</Nav.Link></Nav.Item>              
+                <Nav.Item><Nav.Link href="/">{context.user.user_name}</Nav.Link></Nav.Item>
             }
+            ///////
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Login</Modal.Title>
@@ -92,20 +93,20 @@ const Login = () => {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="text" placeholder="Enter email" name="email" value={values.email} onChange={handleChangeEmail}/>
+                            <Form.Control type="text" placeholder="Enter email" name="email" value={values.email} onChange={handleChangeEmail} />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" name="password" value={values.password} onChange={handleChangePassword}/>
+                            <Form.Control type="password" placeholder="Password" name="password" value={values.password} onChange={handleChangePassword} />
                         </Form.Group>
                         <Button variant="primary" type="submit">
-                        Login
+                            Login
                     </Button>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    
+
                 </Modal.Footer>
             </Modal>
         </Styles>

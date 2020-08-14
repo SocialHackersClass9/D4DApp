e06679cd97e23b23e804551aa02f2971cf9fe6c1
Evina Mouselimi <evinamous@gmail.com>
2020-08-14 14:42:58 +0300

    Uninstalled all packages. Cookie and GDPR consent pop-up

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -4,11 +4,10 @@ import Carousel from '../Components/Carousel';
 import ThreeColumnSection from '../Components/ThreeColumnSection';
 import MidSection from '../Components/MidSection';
 import Inspirational from '../Components/Inspirational';
-import CookieConsentAlert from './CookieConsent';
+
 
 const home = () => (
     <div className="container-fluid">
-        <CookieConsentAlert />
         <Navigation />
         <Carousel />
         <ThreeColumnSection />

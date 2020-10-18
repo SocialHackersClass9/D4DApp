44e4e13d653729a1047373bd9456de0268404612
Kostas Minaidis <minaidisk@gmail.com>
2020-10-18 03:54:49 +0300

    Updating env.js with proper path traversal and fixing value in template literals

--- backend/env.js
+++ backend/env.js
@@ -1,11 +1,12 @@
 const appRootPath = require('app-root-path');
 const fileExists = require('file-exists');
 const env = require('node-env-file');
+const path = require('path');
 
 module.exports = {
     get: () => {
-        const envFileLocation = appRootPath + '/.env.local';
-        console.log(`envFileLocation={envFileLocation}`);
+        const envFileLocation = path.join( appRootPath.path, 'env.local' );
+        console.log(`envFileLocation=${envFileLocation}`);
         if (fileExists.sync(envFileLocation)) {
             env(envFileLocation);
         }
